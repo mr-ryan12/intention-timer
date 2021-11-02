f87fc31b47ce0573e9294d0f990f94151bd36f39
Ryan McBride <mr.ryan444@gmail.com>
2021-11-02 17:04:17 -0600

    Add formatting to study button

--- main.js
+++ main.js
@@ -8,3 +8,22 @@ var startActivityButton = document.querySelector('.start-button');
 var accomplishInput = document.querySelector('.accomplish-input');
 var minutesInput = document.querySelector('.minutes-input');
 var secondsInput = document.querySelector('.second-input');
+
+studyButton.addEventListener('click', changeStudyButtonColor);
+// meditateButton.addEventListener('click', changeColor);
+// exerciseButton.addEventListener('click', changeColor);
+
+function changeStudyButtonColor() {
+  studyButton.style.background = "url('assets/study-active.svg')";
+  studyButton.style.color = "#B3FD78";
+  studyButton.style.backgroundRepeat = "no-repeat";
+  studyButton.style.backgroundSize = "25px 25px";
+  studyButton.style.backgroundPosition = "58px 10px";
+  studyButton.style.height = "67px";
+  studyButton.style.width = "142px";
+  studyButton.style.border = "1px solid #B3FD78";
+  studyButton.style.paddingLeft = "10px";
+  studyButton.style.paddingRight = "10px";
+  studyButton.style.paddingTop = "27px";
+  studyButton.style.borderRadius = "5px 5px";
+}
