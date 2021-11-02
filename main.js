fa875d6f99c7663e8657732560f1f37a90c64255
Ryan McBride <mr.ryan444@gmail.com>
2021-11-02 17:24:10 -0600

    Add styling to buttons

--- main.js
+++ main.js
@@ -9,21 +9,30 @@ var accomplishInput = document.querySelector('.accomplish-input');
 var minutesInput = document.querySelector('.minutes-input');
 var secondsInput = document.querySelector('.second-input');
 
-studyButton.addEventListener('click', changeStudyButtonColor);
-// meditateButton.addEventListener('click', changeColor);
-// exerciseButton.addEventListener('click', changeColor);
+studyButton.addEventListener('click', addStudyButtonColor);
+meditateButton.addEventListener('click', addMeditateButtonColor);
+exerciseButton.addEventListener('click', addExerciseButtonColor);
 
-function changeStudyButtonColor() {
-  studyButton.style.background = "url('assets/study-active.svg')";
-  studyButton.style.color = "#B3FD78";
-  studyButton.style.backgroundRepeat = "no-repeat";
-  studyButton.style.backgroundSize = "25px 25px";
-  studyButton.style.backgroundPosition = "58px 10px";
-  studyButton.style.height = "67px";
-  studyButton.style.width = "142px";
-  studyButton.style.border = "1px solid #B3FD78";
-  studyButton.style.paddingLeft = "10px";
-  studyButton.style.paddingRight = "10px";
-  studyButton.style.paddingTop = "27px";
-  studyButton.style.borderRadius = "5px 5px";
+function addStudyButtonColor() {
+  studyButton.classList.add('study-button-active');
+}
+
+function removeStudyButtonColor() {
+  studyButton.classList.remove('study-button-active');
+}
+
+function addMeditateButtonColor() {
+  meditateButton.classList.add('meditate-button-active');
+}
+
+function removeMeditateButtonColor() {
+  meditateButton.classList.remove('meditate-button-active');
+}
+
+function addExerciseButtonColor() {
+  exerciseButton.classList.add('exercise-button-active');
+}
+
+function removeExerciseButtonColor() {
+  exerciseButton.classList.remove('exercise-button-active');
 }
