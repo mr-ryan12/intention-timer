75963f4428dd919a76519532b08b6063a65031c4
Daniel O'Connell <danieloconnell.maker@gmail.com>
2021-11-02 14:04:30 -0700

    Declare button/input variables, add left container views

---
+++ main.js
@@ -0,0 +1,10 @@
+//buttons
+var studyButton = document.querySelector('.study-button');
+var meditateButton = document.querySelector('.meditate-button');
+var exerciseButton = document.querySelector('.exercise-button');
+var startActivityButton = document.querySelector('.start-button');
+
+//inputs
+var accomplishInput = document.querySelector('.accomplish-input');
+var minutesInput = document.querySelector('.minutes-input');
+var secondsInput = document.querySelector('.second-input');
