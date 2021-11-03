09a7bf9923952a0b063cebd3aa273f7bb94d23e0
Ryan McBride <mr.ryan444@gmail.com>
2021-11-02 19:01:44 -0600

    Add functionality to only accept positive numbers

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var studyButton = document.querySelector('.study-button');
 var meditateButton = document.querySelector('.meditate-button');
 var exerciseButton = document.querySelector('.exercise-button');
 var startActivityButton = document.querySelector('.start-button');
+var errorMessage = document.querySelector('.error-message');
 
 //inputs
 var accomplishInput = document.querySelector('.accomplish-input');
@@ -58,8 +59,15 @@ function removeExerciseButtonColor() {
   exerciseButton.classList.remove('exercise-button-active');
 }
 
+function displayErrorMessage() {
+  errorMessage.classList.remove('hidden');
+}
+
 function checkForNumbers(event) {
   event.preventDefault();
+  if ((minutesInput.value === '') || (secondsInput.value === '') || (accomplishInput.value === '')) {
+    displayErrorMessage();
+  }
 }
 
 function preventMinutesE() {
@@ -73,7 +81,7 @@ function preventMinutesE() {
 
 function preventSecondsE() {
   secondsInput.addEventListener("keydown", function(e) {
-    var invalidChars = 'e';
+    var invalidChars = ['e', '-'];
     if (invalidChars.includes(e.key)) {
       e.preventDefault();
     }
